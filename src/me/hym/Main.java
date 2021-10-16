5ad60fabe2c26a564590dc94c2c7260c698c077e
バターネコ <ButterCat_HYM@outlook.com>
2021-10-16 09:01:35 +0800

    init commit

---
+++ src/me/hym/Main.java
@@ -0,0 +1,12 @@
+package me.hym;
+
+public class Main {
+    /**
+     * HYM740 2021-10-16
+     * 程序入口
+     * @param args 从命令行传入的参数
+     */
+    public static void main(String[] args) {
+
+    }
+}
